f35ac7d32a19393771a142a633f1e917bb30e28d
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-12 19:41:26 +0530

    Update Accept.java

--- Hybrid/Accept.java
+++ Hybrid/Accept.java
@@ -1,1 +1,24 @@
+package Arrays.hybrid;
+
+import java.util.Scanner;
+
+public class Accept
+{
+	int f=1,size,i,j,temp,gt,num1;
+	int[] arr;
+	Scanner sc=new Scanner(System.in);
+	public void accept_demo()
+	{
+		System.out.println("Enter the size : ");
+		size=sc.nextInt();
+		arr=new int[size];
+		System.out.println("Enter "+size+" numbers :");
+		for(i=0;i<size;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+	}
+
+}
 
