7d81b1c66a120184cffa1995dbd6690247398d06
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-12 19:43:38 +0530

    Add files via upload

---
+++ Hybrid/Main_Class.java
@@ -0,0 +1,13 @@
+package Arrays.hybrid;
+
+public class Main_Class {
+	public static void main(String[] args)
+	{
+		Fact_No f = new Fact_No();
+		f.cal_fact();
+		f.cal_gr();
+		ASC_demo ob = new ASC_demo();
+		ob.cal_asc();
+	}
+
+}
